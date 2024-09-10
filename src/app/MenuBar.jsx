b8bb1b8415e0b5c48c2f5d67de533715ae7fac33
Aparna Gupta <aparnagupta813@gmail.com>
2024-09-10 18:14:56 +0530

    updates

--- src/app/MenuBar.jsx
+++ src/app/MenuBar.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useContext } from 'react'
 import { newsContext } from './Main'
 
--- src/app/NewsCards.jsx
+++ src/app/NewsCards.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react'
 
 const NewsCards = ({title, description, imagesrc, url}) => {
