cf81d544fb5d0733e8b28d4aa8fa1a83bc02f587
Aparna Gupta <aparnagupta813@gmail.com>
2024-09-10 18:26:12 +0530

    updates

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,21 +1,40 @@
+// "use client"
+// import React from "react";
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+ 
+// import Main from "./Main";
+
+
+
+
+
+// export default function Home() {
+
+//   return (
+//     <div>
+//      <Main />
+//     </div>
+ 
+//   );
+// }
+
+
+
+
+
+
 "use client"
 import React from "react";
+import dynamic from 'next/dynamic';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
- 
-import Main from "./Main";
 
+const Main = dynamic(() => import('./Main'), { ssr: false });
 
-
-
-
-export default function Home() {
-
-  return (
-    <div>
-     <Main />
-    </div>
- 
-  );
+export default function Page() {
+  return <Main />;
 }
+
